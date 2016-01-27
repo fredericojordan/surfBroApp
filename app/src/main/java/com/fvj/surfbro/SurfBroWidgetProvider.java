17551f3ec1353ada31e0a3afbe33f097f050d143
Frederico Jordan <fredericojordan@gmail.com>
2016-01-27 19:42:40 -0200

    First commit

---
+++ app/src/main/java/com/fvj/surfbro/SurfBroWidgetProvider.java
@@ -0,0 +1,13 @@
+package com.fvj.surfbro;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by fvj on 27/01/2016.
+ */
+public class SurfBroWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    }
+}
