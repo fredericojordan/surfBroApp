577fb4e802678b5ddd8ddfe563be92de2d4b3a87
Frederico Jordan <fredericojordan@gmail.com>
2016-02-01 13:55:21 -0200

    Example stack widget

---
+++ app/src/main/java/com/fvj/surfbro/WidgetItem.java
@@ -0,0 +1,12 @@
+package com.fvj.surfbro;
+
+/**
+ * Created by fvj on 01/02/2016.
+ */
+public class WidgetItem {
+    public String text;
+
+    public WidgetItem(String text) {
+        this.text = text;
+    }
+}
